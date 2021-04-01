6c3d722c5c893fb75b9861a81e033041ca9a806f
Paolo Chang <chanhwan.chang@gmail.com>
2021-03-31 20:39:14 -0400

    Add Create Account & Log In screens

--- screens/LogIn.tsx
+++ screens/LogIn.tsx
@@ -1,16 +1,25 @@
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+import AuthButton from "../components/auth/AuthButton";
+import AuthLayout from "../components/auth/AuthLayout";
+import { TextInput } from "../components/auth/AuthShared";
 
 export default function LogIn({ navigation }: any) {
   return (
-    <View>
-      <Text>LogIn</Text>
-
-      <TouchableOpacity onPress={() => navigation.navigate("CreateAccount")}>
-        <View>
-          <Text>Create Account</Text>
-        </View>
-      </TouchableOpacity>
-    </View>
+    <AuthLayout>
+      <TextInput
+        placeholder="Username"
+        placeholderTextColor="rgba(255,255,255,0.5)"
+        returnKeyType="next"
+      />
+      <TextInput
+        placeholder="Password"
+        placeholderTextColor="rgba(255,255,255,0.5)"
+        secureTextEntry
+        returnKeyType="done"
+        lastInput={true}
+      />
+      <AuthButton text="Create Account" disabled={true} onPress={() => null} />
+    </AuthLayout>
   );
 }
