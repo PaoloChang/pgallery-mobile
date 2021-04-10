537840c5b25bbfccf5225dcd316cbaf82471a8c3
Paolo Chang <chanhwan.chang@gmail.com>
2021-04-10 17:10:34 -0400

    Add apollo authLink with token

--- apollo.ts
+++ apollo.ts
@@ -1,29 +1,44 @@
-import { ApolloClient, InMemoryCache, makeVar } from "@apollo/client";
+import {
+  ApolloClient,
+  createHttpLink,
+  InMemoryCache,
+  makeVar,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const isLoggedInVar = makeVar(false);
-export const tokenVar = makeVar("");
+export const isLoggedInVar = makeVar<boolean>(false);
+export const tokenVar = makeVar<string | null>(null);
+
+const TOKEN = "token";
 
 export const logUserIn = async (token: string) => {
-  await AsyncStorage.multiSet([
-    ["token", token],
-    ["loggedIn", "yes"],
-  ]);
+  await AsyncStorage.setItem(TOKEN, token);
   isLoggedInVar(true);
   tokenVar(token);
 };
 
 export const logOutUser = async () => {
-  await AsyncStorage.multiSet([
-    ["token", ""],
-    ["loggedIn", "no"],
-  ]);
+  await AsyncStorage.removeItem(TOKEN);
   isLoggedInVar(false);
-  tokenVar("");
+  tokenVar(null);
 };
 
-const client = new ApolloClient({
+const httpLink = createHttpLink({
   uri: `https://moody-cheetah-87.loca.lt/graphql`,
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      token: tokenVar(),
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
