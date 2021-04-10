83f2499b8cd412e99dc18ef2f8357660e0c49c0b
Paolo Chang <chanhwan.chang@gmail.com>
2021-04-10 17:43:36 -0400

    Add feed mutation

--- screens/Feed.tsx
+++ screens/Feed.tsx
@@ -1,7 +1,32 @@
+import { gql, useQuery } from "@apollo/client";
 import React from "react";
 import { View, Text, TouchableOpacity } from "react-native";
+import { COMMENT_FRAGMENT, PHOTO_FRAGMENT } from "../fragments";
+
+const FEED_QUERY = gql`
+  query seeFeeds {
+    seeFeeds {
+      ...PhotoFragment
+      user {
+        username
+        avatar
+      }
+      caption
+      comments {
+        ...CommentFragment
+      }
+      createdAt
+      isMine
+    }
+  }
+  ${PHOTO_FRAGMENT}
+  ${COMMENT_FRAGMENT}
+`;
 
 const Feed = ({ navigation }: any) => {
+  const { data } = useQuery(FEED_QUERY);
+  console.log(data);
+
   return (
     <View
       style={{
