fd8e3d912632607a58f74e0b7481930e044e2aa2
Paolo Chang <chanhwan.chang@gmail.com>
2021-04-28 23:28:32 -0400

    Add GalleryHeader

--- screens/Gallery.tsx
+++ screens/Gallery.tsx
@@ -1,8 +1,73 @@
+import React, { useEffect } from "react";
 import { RouteProp } from "@react-navigation/core";
 import { StackNavigationProp } from "@react-navigation/stack";
-import React, { useEffect } from "react";
-import { View, Text } from "react-native";
 import { SharedStackParamList } from "../navigators/SharedStackNav";
+import { gql, useQuery } from "@apollo/client";
+import { PHOTO_FRAGMENT } from "../fragments";
+import {
+  View,
+  Image,
+  FlatList,
+  TouchableOpacity,
+  useWindowDimensions,
+} from "react-native";
+import styled from "styled-components/native";
+import GalleryHeader from "../components/GalleryHeader";
+
+const SEE_PROFILE_QUERY = gql`
+  query seeProfile($username: String!) {
+    seeProfile(username: $username) {
+      id
+      firstName
+      lastName
+      username
+      bio
+      avatar
+      photos {
+        ...PhotoFragment
+      }
+      totalFollowing
+      totalFollowers
+      isMine
+      isFollowing
+    }
+  }
+  ${PHOTO_FRAGMENT}
+`;
+
+interface IHeader {
+  username: string;
+  avatar: string;
+}
+
+interface IPhoto {
+  id: number;
+  image: string;
+}
+
+interface IProfile {
+  id: number;
+  firstName: string;
+  lastName: string;
+  username: string;
+  bio: string;
+  avatar: string;
+  photos: Array<{
+    id: number;
+    image: string;
+    likes: number;
+    isLiked: boolean;
+    commentNumber: number;
+  }>;
+  totalFollowing: number;
+  totalFollowers: number;
+  isMine: boolean;
+  isFollowing: boolean;
+}
+
+interface ISeeProfile {
+  seeProfile: IProfile;
+}
 
 type GalleryScreenNavigationProp = StackNavigationProp<
   SharedStackParamList,
@@ -17,6 +82,18 @@ interface Props {
 }
 
 const Gallery: React.FC<Props> = ({ navigation, route: { params } }) => {
+  const numColumns = 3;
+  const { width } = useWindowDimensions();
+  const { data, loading } = useQuery<ISeeProfile>(SEE_PROFILE_QUERY, {
+    variables: {
+      username: params.username,
+    },
+    onCompleted: (data) => {
+      console.log("onCompleted");
+      console.log(data);
+    },
+  });
+
   useEffect(() => {
     if (params.username) {
       navigation.setOptions({
@@ -25,7 +102,34 @@ const Gallery: React.FC<Props> = ({ navigation, route: { params } }) => {
     }
   }, []);
 
-  console.log(navigation, params);
+  const ListHeaderComponent = () => (
+    <GalleryHeader
+      username={data ? data.seeProfile.username : params.username}
+      avatar={data ? data.seeProfile.avatar : ""}
+      bio={data ? data.seeProfile.bio : ""}
+      totalFollowing={data ? data.seeProfile.totalFollowing : 0}
+      totalFollowers={data ? data.seeProfile.totalFollowers : 0}
+      isFollowing={data ? data.seeProfile.isFollowing : false}
+      isMine={data ? data.seeProfile.isMine : false}
+    />
+  );
+
+  const renderItem = ({ item: photo }: { item: IPhoto }) => {
+    return (
+      <TouchableOpacity
+        onPress={() =>
+          navigation.navigate("Photo", {
+            photoId: photo.id,
+          })
+        }
+      >
+        <Image
+          source={{ uri: photo.image }}
+          style={{ height: width / numColumns, width: width / numColumns }}
+        />
+      </TouchableOpacity>
+    );
+  };
 
   return (
     <View
@@ -36,7 +140,14 @@ const Gallery: React.FC<Props> = ({ navigation, route: { params } }) => {
         justifyContent: "center",
       }}
     >
-      <Text style={{ color: "white" }}>Someone's Gallery</Text>
+      <FlatList
+        data={data?.seeProfile.photos}
+        renderItem={renderItem}
+        keyExtractor={(photo) => photo.id.toString()}
+        numColumns={numColumns}
+        ListHeaderComponent={ListHeaderComponent}
+        style={{ width: "100%" }}
+      />
     </View>
   );
 };
