a8e1fbd985599a911f5ea6e8fed60f2bd54a89ba
Paolo Chang <chanhwan.chang@gmail.com>
2021-04-29 22:46:13 -0400

    Fix GalleryHeader

--- apollo.ts
+++ apollo.ts
@@ -26,7 +26,7 @@ export const logOutUser = async () => {
 };
 
 const httpLink = createHttpLink({
-  uri: `https://tidy-newt-88.loca.lt/graphql`,
+  uri: `https://heavy-deer-36.loca.lt/graphql`,
 });
 
 const authLink = setContext((_, { headers }) => {
